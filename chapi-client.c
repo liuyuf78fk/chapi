6616bfe868c95023a17501d608542ea9a7ff8636
Liu Yu <f78fk@live.com>
2025-06-29 08:32:14 +0900

    Improve error handling and ensure C89 compatibility in the client code. Refactor hex_to_bin and variable declarations for better portability.
    Signed-off-by: Liu Yu <f78fk@live.com>

--- chapi-client.c
+++ chapi-client.c
@@ -33,39 +33,57 @@
 
 void hex_to_bin(const char *hex, unsigned char *bin, size_t bin_len)
 {
-	for (size_t i = 0; i < bin_len; i++) {
-		sscanf(hex + 2 * i, "%2hhx", &bin[i]);
+	size_t i;
+	unsigned int byte;
+	for (i = 0; i < bin_len; i++) {
+		sscanf(hex + 2 * i, "%2x", &byte);
+		bin[i] = (unsigned char)byte;
 	}
 }
 
 int is_valid_ipv4(const char *ip)
 {
 	struct sockaddr_in sa;
-	return inet_pton(AF_INET, ip, &(sa.sin_addr)) == 1;
+	int result = inet_pton(AF_INET, ip, &(sa.sin_addr));
+	return result == 1 ? 1 : 0;
 }
 
-int main()
+int main(void)
 {
+	int sockfd = -1;
+	unsigned char *sendbuf = NULL;
+	int ret = -1;
+	ssize_t sent = -1;
+	ssize_t bytes_received = -1;
+
 	if (sodium_init() < 0) {
 		fprintf(stderr, "libsodium init failed\n");
-		return 1;
+		goto err;
 	}
 
-	int sockfd;
-	struct sockaddr_in servaddr;
+	if (strlen(KEY_HEX) != KEY_LEN * 2) {
+		fprintf(stderr,
+			"KEY_HEX length is invalid, expected %d hex digits.\n",
+			KEY_LEN * 2);
+		goto err;
+	}
 
 	unsigned char key[KEY_LEN];
 	hex_to_bin(KEY_HEX, key, KEY_LEN);
 
 	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		perror("socket creation failed");
-		exit(EXIT_FAILURE);
+		goto err;
 	}
 
+	struct sockaddr_in servaddr;
 	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(SERVER_PORT);
-	inet_pton(AF_INET, SERVER_PUBLIC_ADDR, &servaddr.sin_addr);
+	if (inet_pton(AF_INET, SERVER_PUBLIC_ADDR, &servaddr.sin_addr) <= 0) {
+		perror("inet_pton failed");
+		goto err;
+	}
 
 	unsigned char nonce[NONCE_LEN];
 	randombytes_buf(nonce, NONCE_LEN);
@@ -74,37 +92,60 @@ int main()
 	unsigned char ciphertext[MAX_MSG_LEN];
 	unsigned long long clen;
 
-	crypto_aead_chacha20poly1305_ietf_encrypt(ciphertext, &clen,
-						  (const unsigned char *)msg,
-						  strlen(msg), NULL, 0, NULL,
-						  nonce, key);
+	if (crypto_aead_chacha20poly1305_ietf_encrypt(ciphertext, &clen,
+						      (const unsigned char *)
+						      msg, strlen(msg), NULL, 0,
+						      NULL, nonce, key) != 0) {
+		fprintf(stderr, "Encryption failed\n");
+		goto err;
+	}
 
-	unsigned char sendbuf[NONCE_LEN + clen];
+	sendbuf = malloc(NONCE_LEN + clen);
+
+	if (sendbuf == NULL) {
+		perror("malloc failed");
+		goto err;
+	}
+
 	memcpy(sendbuf, nonce, NONCE_LEN);
 	memcpy(sendbuf + NONCE_LEN, ciphertext, clen);
 
-	sendto(sockfd, sendbuf, sizeof(sendbuf), 0,
-	       (const struct sockaddr *)&servaddr, sizeof(servaddr));
+	sent = sendto(sockfd, sendbuf, NONCE_LEN + clen, 0,
+		      (const struct sockaddr *)&servaddr, sizeof(servaddr));
+
+	if (sent < 0) {
+		perror("sendto failed");
+		goto err;
+	}
+
+	free(sendbuf);
+	sendbuf = NULL;
 
 	struct timeval timeout = { 2, 0 };
-	setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+	if (setsockopt
+	    (sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+		perror("setsockopt failed");
+		goto err;
+	}
 
 	unsigned char recvbuf[MAX_MSG_LEN];
-	ssize_t n = recvfrom(sockfd, recvbuf, sizeof(recvbuf), 0, NULL, NULL);
 
-	if (n < 0) {
+	bytes_received =
+	    recvfrom(sockfd, recvbuf, sizeof(recvbuf), 0, NULL, NULL);
+
+	if (bytes_received < 0) {
 		if (errno == EAGAIN || errno == EWOULDBLOCK) {
 			fprintf(stderr, "The server is unresponsive\n");
 		} else {
-			perror("recvfrom error\n");
+			perror("recvfrom error");
 		}
-		return 1;
+		goto err;
 	}
 
-	if (n < NONCE_LEN) {
+	if (bytes_received < NONCE_LEN) {
 		fprintf(stderr,
 			"Received packet is too short, invalid format.\n");
-		return 1;
+		goto err;
 	}
 
 	memcpy(nonce, recvbuf, NONCE_LEN);
@@ -113,19 +154,32 @@ int main()
 
 	if (crypto_aead_chacha20poly1305_ietf_decrypt(decrypted, &mlen, NULL,
 						      recvbuf + NONCE_LEN,
-						      n - NONCE_LEN, NULL, 0,
-						      nonce, key) != 0) {
+						      bytes_received -
+						      NONCE_LEN, NULL, 0, nonce,
+						      key) != 0) {
 		fprintf(stderr, "Decryption failed\n");
-		return 1;
+		goto err;
 	}
 
-	decrypted[mlen] = '\0';
-	if (is_valid_ipv4((char *)decrypted)) {
-		printf("%s\n", decrypted);
-	} else {
+	decrypted[mlen] = '\0';	// safe: mlen always < MAX_MSG_LEN due to AEAD max length
+	if (!is_valid_ipv4((char *)decrypted)) {
 		fprintf(stderr, "Invalid IP: %s\n", decrypted);
-		return 1;
+		goto err;
 	}
 
-	return 0;
+	if (strcmp((char *)decrypted, "0.0.0.0") == 0) {
+		fprintf(stderr,
+			"Server returned 0.0.0.0 - failed to determine client IP address.\n");
+		goto err;
+	}
+
+	printf("%s\n", decrypted);
+	ret = 0;
+
+ err:
+	if (sendbuf)
+		free(sendbuf);
+	if (sockfd >= 0)
+		close(sockfd);
+	return ret;
 }
